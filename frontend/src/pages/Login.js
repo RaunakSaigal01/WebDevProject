1399bf7bff59bde724fba7a2ca36193d8bab919d
RaunakSaigal01 <raunaksaigal01@gmail.com>
2025-03-31 22:37:59 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,7 +9,6 @@ function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    // Send login request to backend
     const response = await fetch("http://localhost:5000/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
